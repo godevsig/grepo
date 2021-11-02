19006d61c2c1d236238aa9414924cb98e769d329
Bai Yingjie <byj.tea@gmail.com>
2021-11-02 10:18:38 +0000

    topidchart: adjust log

--- app-chart/topid/topidchart.go
+++ app-chart/topid/topidchart.go
@@ -10,22 +10,15 @@ import (
 	"github.com/godevsig/grepo/srv-chart/topid"
 )
 
-var (
-	dir       string
-	port      string
-	parsefile string
-	logLevel  string
-)
-
 var server *topid.DataServer
 
 // Start starts the service
 func Start(args []string) (err error) {
 	flags := flag.NewFlagSet("", flag.ContinueOnError)
-	flags.StringVar(&logLevel, "logLevel", "info", "set log level")
-	flags.StringVar(&dir, "dir", "topidata", "set directory for saving topid raw data")
-	flags.StringVar(&port, "port", "9998", "set port for visiting chart http server")
-	flags.StringVar(&parsefile, "parse", "", "parse file")
+	logLevel := flags.String("logLevel", "info", "set log level")
+	dir := flags.String("dir", "topidata", "set directory for saving topid raw data")
+	port := flags.String("port", "9998", "set port for visiting chart http server")
+	parsefile := flags.String("parse", "", "parse file")
 
 	if err := flags.Parse(args); err != nil {
 		if err == flag.ErrHelp {
@@ -34,33 +27,21 @@ func Start(args []string) (err error) {
 		return err
 	}
 
-	if len(parsefile) != 0 {
-		topid.Parse(parsefile)
+	if len(*parsefile) != 0 {
+		topid.Parse(*parsefile)
 		return nil
 	}
 
 	stream := log.NewStream("")
-	defer stream.Close()
 	stream.SetOutputter(os.Stdout)
-	level := log.Linfo
-	switch logLevel {
-	case "debug":
-		level = log.Ldebug
-	case "info":
-		level = log.Linfo
-	case "warn":
-		level = log.Lwarn
-	case "error":
-		level = log.Lerror
-	}
-	lg := stream.NewLogger("topidchart", level)
-	defer lg.Close()
+	lg := stream.NewLogger("topidchart", log.StringToLoglevel(*logLevel))
 
-	server := topid.NewServer(lg, port, dir)
+	fmt.Println("topid chart server starting...")
+	server := topid.NewServer(lg, *port, *dir)
 	if server == nil {
 		return errors.New("create topid chart server failed")
 	}
-	fmt.Println("topid chart server starting...")
+
 	server.Start()
 
 	return nil
